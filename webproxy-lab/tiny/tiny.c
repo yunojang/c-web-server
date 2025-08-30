274036482a57512773266687e45d18579cd7279b
yunojang <hanganda23@naver.com>
2025-08-30 22:05:39 +0900

    fe: tiny main connect, doit 일부

--- webproxy-lab/tiny/tiny.c
+++ webproxy-lab/tiny/tiny.c
@@ -17,30 +17,71 @@ void serve_dynamic(int fd, char *filename, char *cgiargs);
 void clienterror(int fd, char *cause, char *errnum, char *shortmsg,
                  char *longmsg);
 
+void read_requesthdrs(rio_t *rp)
+{
+  char buf[MAXLINE];
+
+  while (strcmp(buf, "\r\n"))
+  {
+    rio_readlineb(rp, buf, MAXLINE);
+    printf("headers: %s", buf);
+  }
+
+  return;
+}
+
+// 한개의 http 트랜잭션 처리 함수
+void doit(int fd)
+{
+  char buf[MAXLINE];
+  rio_t rio;
+
+  // int n;
+  rio_readinitb(&rio, fd);
+  rio_readlineb(&rio, buf, MAXLINE);
+  printf("Request headers:\n");
+  printf("%s", buf);
+
+  char method[10], url[MAXLINE], version[10];
+  sscanf(buf, "%s %s %s\n", method, url, version);
+  printf("%s %s %s\n", method, url, version);
+  if (!method || strcasecmp(method, "GET")) // get, GET
+  {
+    fprintf(stderr, "GET method allow: [%s]\n", method);
+    return;
+  }
+  read_requesthdrs(&rio);
+}
+
 int main(int argc, char **argv)
 {
-  int listenfd, connfd;
-  char hostname[MAXLINE], port[MAXLINE];
-  socklen_t clientlen;
-  struct sockaddr_storage clientaddr;
-
-  /* Check command line args */
   if (argc != 2)
   {
-    fprintf(stderr, "usage: %s <port>\n", argv[0]);
+    fprintf(stderr, "Usage: %s <port>\n", argv[0]);
     exit(1);
   }
 
-  listenfd = Open_listenfd(argv[1]);
+  char *port = argv[1];
+  int listenfd;
+  if ((listenfd = open_listenfd(port)) < 0)
+  {
+    perror("open_listenfd");
+    exit(1);
+  }
+
+  int connfd;
+  struct sockaddr_storage client_addr;
+  char host[MAXLINE], serv[MAXLINE];
   while (1)
   {
-    clientlen = sizeof(clientaddr);
-    connfd = Accept(listenfd, (SA *)&clientaddr,
-                    &clientlen); // line:netp:tiny:accept
-    Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE,
-                0);
-    printf("Accepted connection from (%s, %s)\n", hostname, port);
-    doit(connfd);  // line:netp:tiny:doit
-    Close(connfd); // line:netp:tiny:close
+    fprintf(stdout, "Wating client...\n");
+
+    socklen_t clientaddr_len;
+    connfd = accept(listenfd, (SA *)&client_addr, &clientaddr_len);
+    getnameinfo((SA *)&client_addr, clientaddr_len, host, MAXLINE, serv, MAXLINE, 0);
+    fprintf(stdout, "Connected (%s : %s)\n", host, serv);
+
+    doit(connfd);
+    close(connfd);
   }
 }

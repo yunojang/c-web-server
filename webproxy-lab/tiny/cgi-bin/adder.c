65761d600d1933438870fe386052f6d573081ac4
yunojang <hanganda23@naver.com>
2025-09-01 20:09:18 +0900

    fix: adder args

--- webproxy-lab/tiny/cgi-bin/adder.c
+++ webproxy-lab/tiny/cgi-bin/adder.c
@@ -11,18 +11,23 @@ int main(void)
   int n1 = 0, n2 = 0;
 
   /* Extract the two arguments */
-  if ((buf = getenv("QUERY_STRING")) != NULL)
+  if ((buf = getenv("QUERY_STRING")) == NULL)
   {
-    p = strchr(buf, '&');
-    *p = '\0';
-    strcpy(arg1, buf);
-    strcpy(arg2, p + 1);
-    n1 = atoi(strchr(arg1, '=') + 1);
-    n2 = atoi(strchr(arg2, '=') + 1);
+    return -1;
   }
 
-  /* Make the response body */
   sprintf(content, "QUERY_STRING=%s\r\n<p>", buf);
+
+  p = strchr(buf, '&');
+  *p = '\0';
+  strcpy(arg1, buf);
+  strcpy(arg2, p + 1);
+  n1 = atoi(arg1);
+  n2 = atoi(arg2);
+  // n1 = atoi(strchr(arg1, '=') + 1);
+  // n2 = atoi(strchr(arg2, '=') + 1);
+
+  /* Make the response body */
   sprintf(content + strlen(content), "Welcome to add.com: ");
   sprintf(content + strlen(content), "THE Internet addition portal.\r\n<p>");
   sprintf(content + strlen(content), "The answer is: %d + %d = %d\r\n<p>",
